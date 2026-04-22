5a05e9644ef6ca7fc01a37f5cb8db26f51379d41
Nina Zink <nina.zink2000@web.de>
2026-04-22 14:15:31 +0200

    simplify main to test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-from fintraffic_api.api_test import fetch_live_trains, format_trains
-from llm_engine.llm_test import run_llm
+from pipeline_1 import AssistantPipeline
+
 
 if __name__ == "__main__":
-    trains = format_trains(fetch_live_trains())
-    run_llm(trains)
+    print("Hallo")
