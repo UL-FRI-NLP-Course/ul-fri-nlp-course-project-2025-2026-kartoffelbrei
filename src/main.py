09bcffca01a4d0bed6da283d35fd6e9904e9bf98
Nina Zink <nina.zink2000@web.de>
2026-04-22 10:16:34 +0200

    main für simple tests modifiziert

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from src.backend.metadata_handler import MetadataHandler
+from src.llm_engine.model_pipeline import AssistantPipeline
 
 if __name__ == "__main__":
     print("Hallo")
 
+    pipeline = AssistantPipeline()
+    print("pipeline erfolgreich geladen")
     save = MetadataHandler()
 
     save.save_stations()
