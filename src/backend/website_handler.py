54892c1f12eb8b8f1bd0cbe2ebe8a25fb33baf17
cornelius <cornelius.niese@gmail.com>
2026-04-23 16:16:12 +0200

    maybe fix imports

--- src/backend/website_handler.py
+++ src/backend/website_handler.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from config_backend import ConfigBackend
+from .config_backend import ConfigBackend
 from pathlib import Path
 import json
 from langchain_core.documents import Document
