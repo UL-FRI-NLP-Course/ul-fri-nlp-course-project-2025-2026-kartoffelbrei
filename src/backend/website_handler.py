8e458889c2c0e5f5433064272a41c54fd1011c22
Nina Zink <nina.zink2000@web.de>
2026-04-25 13:01:07 +0200

    make Website handler a class

--- src/backend/website_handler.py
+++ src/backend/website_handler.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from config_backend import ConfigBackend
+from llm_engine.model_manager import ModelManager
 from pathlib import Path
 import json
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter, TokenTextSplitter
+from langchain_community.vectorstores import FAISS
 
+    
 def scrape_website(url: str):
     print(f"Scrape: {url}")
 
@@ -22,10 +25,6 @@ def scrape_website(url: str):
         'source': url
     }
 
-def save_data(all_docs: list[str]):
-    
-    output_path = "src/metadata"
-    # hier noch implementieren dass all_docs gespeichert wird
 
 def create_database():
     websites = ConfigBackend.URLS
@@ -57,12 +56,36 @@ def text_preparation(all_texts: list[dict]):
     )
     chunks = text_splitter.split_documents(documents)
 
+    print(chunks[0])
+
+
     print(f"Documents → chunks: {len(documents)} → {len(chunks)}")
     print(f"Average chunk size : {sum(len(c.page_content) for c in chunks) / len(chunks):.0f} chars")
     print(f"Min / Max          : {min(len(c.page_content) for c in chunks)} / {max(len(c.page_content) for c in chunks)} chars")
     
 
+def vectorize_and_store(chunks, embedding_model):
 
+    vectorstore = FAISS.from_documents(chunks, embedding_model)
+    return vectorstore
+
+def save_faiss_local(vectorestore, path="src/data/faiss_index"):
+    vectorestore.save_local(path)
+    print(f"FAISS-Index saved to {path}")
+
+def load_faiss_local(embedding_model, path="src/data/faiss_index"):
+    print(f"Load FAISS index from {path}")
+    return FAISS.load_local(path, embedding_model, allow_dangerous_deserialization=True)
+
+def search_similiar(vectorstore: FAISS, user_input, k: int=5):
+    results = vectorstore.similarity_search_with_score(user_input, k=k)
+
+    print(f"Top {k} results for this query: {user_input}")
+    for i, (doc, score) in enumerate(results,1):
+        source = doc.metadata.get('source', 'unbekannt')
+        print(f"{i}. Score: {score:.4f} | Quelle: {source}")
+        print(f"   Text: {doc.page_content[:100]}...\n")
+    return results
 
 
 if __name__ == '__main__':
