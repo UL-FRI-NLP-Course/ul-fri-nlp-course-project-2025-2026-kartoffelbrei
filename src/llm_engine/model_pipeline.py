fc18ade83ab75014a4a7301a3119108208eb5a7a
cornelius <cornelius.niese@gmail.com>
2026-04-30 00:46:35 +0200

    fix input length

--- src/llm_engine/model_pipeline.py
+++ src/llm_engine/model_pipeline.py
@@ -88,7 +88,7 @@ class AssistantPipeline:
             pad_token_id=self.model_manager.answer_tokenizer.eos_token_id
         )
 
-        input_length = inputs.shape[1]
+        input_length = inputs["input_ids"].shape[1]
         generated = outputs[0][input_length:]
 
         response = self.model_manager.answer_tokenizer.decode(generated, skip_special_tokens=True)
