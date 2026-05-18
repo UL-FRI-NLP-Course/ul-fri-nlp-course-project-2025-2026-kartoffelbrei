424521030776f91fab2b862b2f559a7e9a524871
Nina-Sophie Zink <nz60828@hpc-login3.arnes.si>
2026-05-18 15:01:40 +0200

    build better prompt

--- src/backend/config_backend.py
+++ src/backend/config_backend.py
@@ -6,5 +6,12 @@ class ConfigBackend():
     TRAINS: str = BASE + "trains"
     LIVE_TRAINS: str = BASE + "live-trains"
     LIVE_TRAINS_STATION: str = BASE + "live-trains/station"
-    URLS = ["https://www.vr.fi/en/terms-and-conditions", "https://www.vr.fi/en/accessibility-statement" 
+    URLS = ["https://www.vr.fi/en/terms-and-conditions", 
+    "https://www.vr.fi/en/accessibility-statement", 
+    "https://www.finlandtrains.com/vr-trains", 
+    "https://www.finlandtrains.com/pendolino-trains",
+    "https://www.finlandtrains.com/intercity-trains",
+     "https://www.finlandtrains.com/night-trains", 
+     "https://www.vr.fi/en/carcarrier", 
+     "https://en.wikipedia.org/wiki/Rail_transport_in_Finland" 
     ]
--- src/llm_engine/config_llm.py
+++ src/llm_engine/config_llm.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class ConfigLLM:
     INTENT_MODEL: str = "mistralai/Mistral-7B-Instruct-v0.3"
-    ANSWER_MODEL: str = "meta-llama/Llama-3.1-8B-Instruct"
+    ANSWER_MODEL: str = "Qwen/Qwen2.5-14B"
     EMBEDDING_MODEL: str = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
     MODEL_CACHE_DIR: str = "/d/hpc/projects/onj_fri/kartoffelbrei/models" 
 
+                                                          
