a4ac307cf623c4b6b02274d91d3c4a0516c385ec
cornelius <cornelius.niese@gmail.com>
2026-05-19 10:11:55 +0200

    change main.py back to normal

--- src/main.py
+++ src/main.py
@@ -1,30 +1,19 @@
 import os
-import dateparser
-
 from pathlib import Path
 from backend.metadata_handler import MetadataHandler
 from llm_engine.model_pipeline import AssistantPipeline
 from backend.api_requests import APIRequests
-from backend.format_response import ResponseFormatter
-from backend.route_params import RouteParams
-from backend.time_converter import TimeConverter
+from backend.format_response import format_train_type_response, format_route_response
 
 if __name__ == "__main__":
-    requests = APIRequests()
+    # requests = APIRequests()
+    # result = requests.get_route_information(departure_station="HKI", destination_station="TPE")
+    # print(format_route_response(train_data=result, departure_station="HKI", destination_station="TPE"))
+    BASE_DIR = Path(__file__).resolve().parent
+    print(BASE_DIR)
 
-    # show correct times
-    departure_time, departure_date = TimeConverter.convert_time("today at 8am")
-    route_params: RouteParams = {'departure_date': departure_date.isoformat()}
-    result = requests.get_journey_information(departure_station="HKI", destination_station="TPE", params=route_params)
-    print(ResponseFormatter.format_journey_response(train_data=result, departure_station="HKI", destination_station="TPE", current_time=departure_time))
-
-    #BASE_DIR = Path(__file__).resolve().parent
-    #print(BASE_DIR)
-
-    #query_path = os.path.join(BASE_DIR, "llm_queries", "queries.txt")
-    #pipeline = AssistantPipeline()
-    #with open(query_path, "r") as file:
-    #    for query in file:
-    #        pipeline.run(query.strip())
-        
-
+    query_path = BASE_DIR / "llm_queries" / "queries.txt"
+    pipeline = AssistantPipeline()
+    with open(query_path, "r") as file:
+        for query in file:
+            pipeline.run(query.strip())
