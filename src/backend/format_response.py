423da259b654a78a55b8d9e64c1f5b5883a7ab81
cornelius <cornelius.niese@gmail.com>
2026-05-20 11:30:02 +0200

    change import

--- src/backend/format_response.py
+++ src/backend/format_response.py
@@ -4,7 +4,7 @@ from typing import List, Any, Tuple
 from datetime import datetime, timedelta
 
 from backend.metadata_handler import MetadataHandler
-from src.backend.time_converter import TimeConverter
+from backend.time_converter import TimeConverter
 
 
 class ResponseFormatter:
